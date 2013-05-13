f14b686576dcd3aa6ab6d5f8a040dc6f56ee9119
Benjamin Golder <benjamin.j.golder@gmail.com>
2013-05-13 18:29:04 -0400

    added a people app

---
+++ people/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=300)
+    bio = models.TextField( blank=True, null=True)
+    image = models.ImageField(
+            upload_to='images/people',
+            )
+
+
+
+
