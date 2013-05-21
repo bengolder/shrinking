539997ce2b02feab77f8af549ede59abb71cafbc
Benjamin Golder <benjamin.j.golder@gmail.com>
2013-05-21 16:11:09 -0400

    added title to person model

--- people/models.py
+++ people/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=300)
+    title = models.CharField(max_length=300, blank=True, null=True)
     bio = models.TextField( blank=True, null=True)
     image = models.ImageField(
             upload_to='images/people',
